0bbed413f6cd05b359575be3c5e83ce46af9f3bd
Muhammad Amray <muhammadamray@gmail.com>
2023-10-01 13:31:09 -0400

    login signup

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,7 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
-const rootReducer = combineReducers({});
+import session from "./session";
+
+const rootReducer = combineReducers({
+  session,
+});
 
 let enhancer;
 
